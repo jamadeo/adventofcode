1d192a36b3907ddfbcb80087791930fdb9a9c8d2
Jack Amadeo <jack@sidewalklabs.com>
2020-12-10 16:59:09 -0500

    Day 10

--- day_10/adapter_array.py
+++ day_10/adapter_array.py
@@ -1,17 +1,9 @@
+from collections import Counter
 import fileinput
 
 
 sequence = [int(l.rstrip()) for l in fileinput.input()]
-sequence = [0] + sorted(sequence) + [max(sequence)]
-seq_len = len(sequence)
-num_paths = [0] * seq_len
-num_paths[0] = 1
+sequence = [0] + sorted(sequence) + [max(sequence) + 3]
 
-for i, val in enumerate(sequence):
-    for offset in (1, 2, 3):
-        if i + offset < seq_len and sequence[i + offset] - val <= 3:
-            num_paths[i + offset] += num_paths[i]
-
-
-print(sequence)
-print(num_paths)
+counts = Counter(b - a for a, b in zip(sequence, sequence[1:]))
+print(counts[1] * counts[3])
