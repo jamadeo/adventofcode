e91977549e3393792b80b6beb04a3ccaf46c4fac
Jack Amadeo <jamadeo@gmail.com>
2020-12-13 19:28:26 -0500

    unnecessary sort

--- day_13/shuttle_search_part_2.py
+++ day_13/shuttle_search_part_2.py
@@ -7,15 +7,13 @@ _timestamp, bus_ids_str = fileinput.input()
 t = 0
 product = 1
 
-pairs = [(int(b_id), offset) for offset, b_id in enumerate(bus_ids_str.split(',')) if b_id != 'x']
-pairs_sorted = sorted(pairs)
+for offset, b_id in enumerate(bus_ids_str.split(',')):
+    if b_id == 'x':
+        continue
 
-for b_id, offset in pairs_sorted:
     b_id = int(b_id)
-
     while (t + offset) % b_id != 0:
         t += product
-
     product *= b_id
 
 
