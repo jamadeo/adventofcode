4775e0899be99763cd80e9511c5ebd03d23e505b
Jack Amadeo <jamadeo@gmail.com>
2020-12-15 13:20:13 -0500

    Day 2

--- day_14/docking_data_part_2.py
+++ day_14/docking_data_part_2.py
@@ -1,5 +1,7 @@
 import fileinput
-from itertools import zip_longest
+from itertools import zip_longest, combinations
+from functools import lru_cache, reduce
+import time
 
 
 def size(mask):
@@ -8,7 +10,10 @@ def size(mask):
     return 2 ** sum(1 for c in mask if c == 'X')
 
 
-def intersection(a, b):
+@lru_cache
+def intersection_pair(a, b):
+    if a is None or b is None:
+        return None
     assert len(a) == len(b)
     ixn = []
     for d_a, d_b in zip(a, b):
@@ -23,6 +28,22 @@ def intersection(a, b):
     return ''.join(ixn)
 
 
+def intersection(*masks):
+    return reduce(intersection_pair, masks)
+
+
+def size_intersection_with_union(mask, others):
+    """Return the intersection of 'mask' with the union of 'others'"""
+    if not others:
+        return 0
+    ixn_size = sum(size(intersection(mask, other)) for other in others)
+    for k in range(2, len(others) + 1):
+        sign = -1 if k % 2 == 0 else 1
+        for combination in combinations(others, k):
+            ixn_size += sign * size(intersection(*combination))
+    return ixn_size
+
+
 def combine(mask, val):
     """Combine a mask with a value"""
     val_mask = f'{val:b}'
@@ -35,8 +56,17 @@ def combine(mask, val):
     return ''.join(reversed(combined))
 
 
+def addresses(mask):
+    x_ix = [ix for ix, l in enumerate(mask) if l == 'X']
+    mask_l = list(mask)
+    for n in range(2 ** len(x_ix)):
+        for i, ix in enumerate(x_ix):
+            mask_l[ix] = str(int(bool(1 << i & n)))
+        yield ''.join(mask_l)
+
+
 if __name__ == '__main__':
-    memory = []
+    memory = {}
 
     running_sum = 0
     for line in fileinput.input():
@@ -47,18 +77,10 @@ if __name__ == '__main__':
             val = int(val)
             mem_addr = int(cmd[4:-1])
             m = combine(mask, mem_addr)
-            running_sum += size(m) * val
-            memory.append((m, val))
-
-    print(running_sum)
+            for address in addresses(m):
+                memory[address] = val
 
     from pprint import pprint
     pprint(memory)
 
-    later_masks = []
-    for mask, value in reversed(memory):
-        if later_masks:
-            running_sum -= total_intersection(mask, later_masks) * value
-        later_masks.append(mask)
-
-    print(running_sum)
+    print(sum(memory.values()))
